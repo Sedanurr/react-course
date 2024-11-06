8d778d07131026ce72a96fe728932cfaab5f2918
Sedanur <sedanurinan@gmail.com>
2024-11-06 20:03:05 +0300

    set loading and error state

--- 06-Lesson/react-async/src/App.js
+++ 06-Lesson/react-async/src/App.js
@@ -13,25 +13,32 @@ function GithubUser({ name, location, avatar }) {
 }
 
 function App() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null)
 
   useEffect(() => {
+    setLoading(true)
     fetch(
       `https://api.github.com/users/Sedanurr`
     )
       .then((response) => response.json())
-      .then(setData);
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError)
   }, []);
-  if (data)
-    return (
-      <GithubUser
-        name={data.name}
-        location={data.location}
-        avatar={data.avatar_url}
-      />
-    );
+
+  if (loading) return <h1>Loading...</h1>
+  if (error)
+    return <pre>{JSON.stringify(error)}</pre>
+  if (!data) return null;
+
   return (
-    <h1>Data</h1>
+    <GithubUser
+      name={data.name}
+      location={data.location}
+      avatar={data.avatar_url}
+    />
   );
 }
 
