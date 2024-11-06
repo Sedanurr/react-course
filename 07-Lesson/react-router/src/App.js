7464bd815fb6b871340f3d9e5dafe4ad99e38d6f
Sedanur <sedanurinan@gmail.com>
2024-11-06 20:45:27 +0300

    prepare components

---
+++ 07-Lesson/react-router/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Home() {
+  return (
+    <h1>Home Page</h1>
+  )
+}
+function About() {
+  return (
+    <p>About Us...</p>
+  )
+}
+function Contact() {
+  return (
+    <h1>Contact Us</h1>
+  )
+}
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
