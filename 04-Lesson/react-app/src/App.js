6f8e5a75006ea4a55bb6a424e6d0203b69659d80
Sedanur <sedanurinan@gmail.com>
2024-11-06 16:12:02 +0300

    useReducer

--- 04-Lesson/react-app/src/App.js
+++ 04-Lesson/react-app/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useReducer } from 'react';
 
 function App() {
-  const [emotion, setEmotion] = useState("happy") // useState bir array ve içinde iki değişken var birincisi app render olduğunda alacağı value, ikincisi ise fonksiyon bu fonksiyonu tanımladığımız state valuesunu güncellemek için kullanıyoruz.
-  const [secondary, setSecondary] = useState("tired")
-
-  useEffect(() => {
-    console.log(`It's ${emotion} right now.`)
-  }, [emotion]) //useEffect'i seçtiğimiz bir state'i izlemek için kullanıyoruz. İki değişken alıyor birincisi fonksiyon ikincisi ise array. Eğer [] verirsek ikinci parametreyi değişken yalnızca ilk render olduğunda izlenir. Bir value verirsek ilgili değişken her değiştiğinde çalışır.
-  useEffect(() => {
-    console.log(`It's ${secondary} right now.`)
-  }, [secondary])
+  const [checked, setChecked] = useReducer((checked) => !checked, false)
   return (
     <div className="App">
-
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>Sad</button>
-      <button onClick={() => setEmotion("excited")}>Excited</button>
-      <h1>Current secondary emotion is {secondary}</h1>
-      <button onClick={() => setSecondary("grateful")}>grateful</button>
+      <input type="checkbox" value={checked} onChange={setChecked} />
+      <label>{checked ? "checked" : "not checked"}</label>
     </div>
   );
 }
