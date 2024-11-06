cde9ca3b62c45a59edbf5c1af4f3a8dc4d10b592
Sedanur <sedanurinan@gmail.com>
2024-11-06 15:45:50 +0300

    useState using

--- 04-Lesson/react-app/src/App.js
+++ 04-Lesson/react-app/src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-const [firstCity, second] = ["tokyo", "istanbul", "berlin"]
-console.log(firstCity)
-console.log(second)
-
-function App({ library }) {
+function App() {
+  const [emotion, setEmotion] = useState("happy") // useState bir array ve içinde iki değişken var birincisi app render olduğunda alacağı value, ikincisi ise fonksiyon bu fonksiyonu tanımladığımız state valuesunu güncellemek için kullanıyoruz.
   return (
     <div className="App">
 
-      <h1>Hello from {library}</h1>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
     </div>
   );
 }
