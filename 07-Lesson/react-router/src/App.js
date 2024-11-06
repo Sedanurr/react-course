a00bb77163fcca544bdb37b7a37f64b71d217fb8
Sedanur <sedanurinan@gmail.com>
2024-11-06 21:00:09 +0300

    router and nested router

--- 07-Lesson/react-router/src/App.js
+++ 07-Lesson/react-router/src/App.js
@@ -1,26 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Link, Outlet } from "react-router-dom";
 
 function Home() {
   return (
-    <h1>Home Page</h1>
+    <div>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>Home Page</h1>
+
+    </div>
+  );
+}
+export function About() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>About Us</h1>
+      <Outlet />
+    </div>
+  );
+}
+export function History() {
+  return (
+    <div>
+      <h1>Our History</h1>
+    </div>
   )
 }
-function About() {
+export function Contact() {
   return (
-    <p>About Us...</p>
-  )
-}
-function Contact() {
-  return (
-    <h1>Contact Us</h1>
-  )
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>Contact Us</h1>
+    </div>
+  );
 }
 
 function App() {
-  return (
-    <Home />
-  );
+  return <Home />;
 }
 
 export default App;
