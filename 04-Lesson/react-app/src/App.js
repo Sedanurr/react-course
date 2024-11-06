f05c412be1c9e1390c228535e750c4321363438d
Sedanur <sedanurinan@gmail.com>
2024-11-06 15:53:01 +0300

    useEffect learned

--- 04-Lesson/react-app/src/App.js
+++ 04-Lesson/react-app/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [emotion, setEmotion] = useState("happy") // useState bir array ve içinde iki değişken var birincisi app render olduğunda alacağı value, ikincisi ise fonksiyon bu fonksiyonu tanımladığımız state valuesunu güncellemek için kullanıyoruz.
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now.`)
+  }, [emotion]) //useEffect'i seçtiğimiz bir state'i izlemek için kullanıyoruz. İki değişken alıyor birincisi fonksiyon ikincisi ise array. Eğer [] verirsek ikinci parametreyi değişken yalnızca ilk render olduğunda izlenir. Bir value verirsek ilgili değişken her değiştiğinde çalışır.
   return (
     <div className="App">
 
